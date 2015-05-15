f660252e26ab60e343ce32bca302aedaaacfdc26
crioo91 <steven.peltier@etu.unistra.fr>
2015-05-15 10:32:07 +0200

    création d'un lien et creation du haut de page avec menu

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var tweets = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Twr' });
+  res.render('index', { title: 'Express', tweets: tweets });
+});
+
+router.post('/', function(req, res, next) {
+    tweets.unshift(req.body.tweet);
+    res.redirect('/')
+});
+
+router.get('/blabla', function(req, res, next) {
+  res.render('blabla', { title: 'Express', tweets: tweets });
+});
+
+router.post('/blabla', function(req, res, next) {
+    tweets.unshift(req.body.tweet);
+    res.redirect('blabla')
 });
 
 module.exports = router;
